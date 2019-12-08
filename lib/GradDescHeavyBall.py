e07cc70fdd4bc49bdb455d06d71c5c2feac34648
Nikita Fedorets <fns1999@yandex.ru>
2019-12-08 15:01:16 +0300

    initial commit for lib

---
+++ lib/GradDescHeavyBall.py
@@ -0,0 +1,5 @@
+from field import Field
+import typing as tp
+
+def grad_desc_heavy_ball(f: tp.Callable[[tp.Sequence[tp.Any]], ] , filed: Field, )
+
---
+++ lib/field.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+class Field:
+    def __init__(self, f, dimX, dimY):
+        self.f = f
+        self.dimX = dimX
+        self.dimY = dimY
