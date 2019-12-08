d9e45f893129e34d4285969f45f698746411996d
nullenstellatch <lesha.vasilevski@mail.ru>
2019-12-08 19:08:18 +0300

    ALMOST GOOD

--- lib/tests_runs/easy_test.py
+++ lib/tests_runs/easy_test.py
@@ -9,8 +9,8 @@ def easy_f(x):
     return x**3 + x
 
 
-def etas(size):
-    return sps.multivariate_normal(mean=[0, 0], cov=[[1, 0], [0, 1]]).rvs(size=size).reshape(size, 2, 1)
+def etas(size, dim):
+    return sps.multivariate_normal(mean=np.zeros(dim), cov=np.identity(dim).rvs(size=size * dim).reshape((size, dim, 1)))
 
 
 def sphere_oracle(x):
@@ -20,14 +20,14 @@ def sphere_oracle(x):
     return x / res
 
 
-def test_on_easy_f(method, steps_num, x_num, graph_name, figname):
+def test_on_f(func, method, steps_num, x_num, graph_name, figname):
     x_0 = np.array([-0.8, 0])
-    my_etas = etas(x_num)
+    my_etas = etas(x_num, 2)
     y_s = np.zeros(x_num)
     for ind, eta in enumerate(my_etas):
         y_s[ind] = np.array(sps.norm.rvs(loc=easy_f(eta.T @ x_0), size=1))
     my_field = field.SignalField(
-        easy_f, x_num, my_etas, y_s.reshape(x_num, 1), 2)
+        func, x_num, my_etas, y_s.reshape(x_num, 1), 2)
     results = method(my_field, np.array(
         [0.3, 0.3]), 1/10, steps_num, sphere_oracle)
     diffs = np.zeros(steps_num)
@@ -39,3 +39,6 @@ def test_on_easy_f(method, steps_num, x_num, graph_name, figname):
     plt.title(graph_name)
     plt.savefig(figname)
     plt.show()
+
+def test_on_easy_f(method, steps_num, x_num, graph_name, figname):
+    test_on_f(easy_f, method, steps_num, x_num, graph_name, figname)
