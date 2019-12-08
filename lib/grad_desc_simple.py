b2cb8bc76b1db3e27d2a94161e198ecc3462e0b3
GPotapov <potapov.ga@phystech.edu>
2019-12-08 18:37:26 +0300

    gds with proj

--- lib/grad_desc_simple.py
+++ lib/grad_desc_simple.py
@@ -4,8 +4,12 @@ import numpy as np
 def _search_argmin_simple_GD(f, x0, mode='precision', L=None, **kwargs):
     if L == None:
         L = np.linalg.norm(f(x0))
-    x = x0
+
+    proj = kwargs['proj_oracle']
+    x = proj(x0)
+
     history = [x.copy()]
+
     criteria = lambda : True
 
     if mode == 'precision':
@@ -20,15 +24,17 @@ def _search_argmin_simple_GD(f, x0, mode='precision', L=None, **kwargs):
         history.append(x.copy())
         L = max(L, np.linalg.norm(f(x)))
         x -= f(x) / (2 * L)
+        x = proj(x)
+
 
     return (x, np.array(history)[1:])
 
 
-def SimpleGDForMonotoneFields(field, start, steps_scale, steps_num):
-    return _search_argmin_simple_GD(field, start, mode='steps', L=steps_scale, **{'steps':steps_num})[1]
+def SimpleGDForMonotoneFields(field, start, steps_scale, steps_num, proj_oracle):
+    return _search_argmin_simple_GD(field, start, mode='steps', L=steps_scale, **{'steps':steps_num, 'proj_oracle':proj_oracle})[1]
 
-if __name__ == '__main__':
-    print(search_argmin_simple_GD((lambda x: x), np.ones(2), mode='precision', **{'precision':1e-4}))
-    print(search_argmin_simple_GD((lambda x: x), np.ones(2), mode='precision', **{'precision':1e-8}))
+#if __name__ == '__main__':
+#    print(search_argmin_simple_GD((lambda x: x), np.ones(2), mode='precision', **{'precision':1e-4}))
+#    print(search_argmin_simple_GD((lambda x: x), np.ones(2), mode='precision', **{'precision':1e-8}))
 
 
