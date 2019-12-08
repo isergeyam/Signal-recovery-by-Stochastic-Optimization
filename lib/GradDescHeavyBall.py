e16a072df5b078127e73f9741f4a1fd9a871c776
Nikita Fedorets <fns1999@yandex.ru>
2019-12-08 16:38:03 +0300

    added heavy ball meathod

--- lib/GradDescHeavyBall.py
+++ lib/GradDescHeavyBall.py
@@ -1,5 +1,16 @@
-from field import Field
+from lib.field import Field
 import typing as tp
+import math
 
-def grad_desc_heavy_ball(f: tp.Callable[[tp.Sequence[tp.Any]], ] , filed: Field, )
 
+def grad_desc_heavy_ball(field: Field, x0: tp.Any, L, steps, projection_oracle, mu = 1) -> tp.Any:
+    k = L / mu
+    alpha = 4 / (math.sqrt(L) + math.sqrt(mu)) ** 2
+    beta  = ((math.sqrt(k) - 1) / (math.sqrt(k) + 1)) ** 2
+    x = x0 - alpha * field(x0)
+    history: tp.List[tp.Any] = [x0, x]
+    for i in range(steps):
+        x = history[-1] + alpha * field(history[-1]) + beta * (history[-1] - history[-2])
+        history.append(x)
+
+    return x, history
