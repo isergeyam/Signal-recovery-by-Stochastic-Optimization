f6d022e755cc41c28bb9a0a39707953b40151ce0
nullenstellatch <lesha.vasilevski@mail.ru>
2019-12-08 20:29:55 +0300

    ALMOST GOOD

--- lib/nesterov.py
+++ lib/nesterov.py
@@ -6,7 +6,7 @@ def NesterovMethod(field, start, steps_scale, steps_num, projection_oracle):
     x = start
     y = start
     for k in range(steps_num):
-        new_x = projection_oracle(y - steps_scale / 2 * field(y))
+        new_x = projection_oracle(y - steps_scale / 10 * field(y))
         y = projection_oracle(new_x + k / (k + 3) * (new_x - x))
         x = new_x
         result.append(x)
--- lib/tests_runs/easy_test.py
+++ lib/tests_runs/easy_test.py
@@ -15,9 +15,9 @@ def etas(size, dim):
 
 def sphere_oracle(x):
     res = np.linalg.norm(x)
-    if res < 1:
+    if res < 2:
         return x
-    return x / res
+    return x / res * 2
 
 def test_on_f(func, method, steps_num, x_num, graph_name, figname):
     x_0 = np.array([-0.8, 0])
@@ -48,6 +48,29 @@ def test_on_logregr(method, steps_num, x_num, graph_name, figname):
     my_field = field.SignalField(log_f, x_num, my_etas, y_s.reshape(x_num, 1), 2)
     results = method(my_field, np.array(
         [0.3, 0.3]), 1/10, steps_num, sphere_oracle)
+    diffs = np.zeros(steps_num)
+    for ind, result in enumerate(results):
+        diffs[ind] = np.linalg.norm(result - x_0)
+    plt.plot(np.arange(steps_num), diffs)
+    plt.xlabel("Количество шагов")
+    plt.ylabel("Отклонение от истинного x")
+    plt.title(graph_name)
+    plt.savefig(figname)
+    plt.show()
+
+def test_on_logreg_dim(method, steps_num, x_num, graph_name, figname, dim):
+    if dim < 4:
+        dim = 4
+    x_0 = np.zeros(dim)
+    x_0[0] = 0.6
+    x_0[3] = 0.2
+    x_0[2] = 0.1
+    my_etas = etas(x_num, dim)
+    y_s = np.zeros(x_num)
+    for ind, eta in enumerate(my_etas):
+        y_s[ind] = np.array(sps.bernoulli.rvs(p=log_f(eta.T @ x_0), size=1))
+    my_field = field.SignalField(log_f, x_num, my_etas, y_s.reshape(x_num, 1), dim)
+    results = method(my_field, np.zeros(dim), 1, steps_num, sphere_oracle)
     diffs = np.zeros(steps_num)
     for ind, result in enumerate(results):
         diffs[ind] = np.linalg.norm(result - x_0)
