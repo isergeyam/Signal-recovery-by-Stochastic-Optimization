3946917c92a1ee3b37dd070cbe0f318b2ef743cb
GPotapov <potapov.ga@phystech.edu>
2019-12-08 15:14:02 +0300

    almost working grad_desc_simple

--- lib/grad_desc_simple.py
+++ lib/grad_desc_simple.py
@@ -1,19 +1,24 @@
 from field import *
 
-def search_min_simple_GD(field, x0=None, precision=1e-6, steps=10000, mode='precision', L=None):
+def search_argmin_simple_GD(field, x0=None, precision=1e-6, steps=10000, mode='precision', L=None):
     if x0 == None:
         x0 = np.zeros(self.dimX)
 
     if L == None:
         L = (lambda f, z: np.linalg.norm(f(z)) / 2)
 
+    x = x0
+
     if mode == 'precision':
         Delta = 2*precision
-        x = x0
         while Delta >= precision:
             x -= field(x) / L(f, x)
 
-        return
+    elif mode == 'steps':
+        for i in range(steps):
+            x -= field(x) / L(f, x)
+
+    return x
 
 
 
